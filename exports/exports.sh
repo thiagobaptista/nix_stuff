f52ea6167755e70a9419df962a7a7b5f7b9962e9
Thiago Baptista <thiago.baptista@concretesolutions.com.br>
2013-08-23 16:09:52 -0300

    Making the script universal

--- exports/exports.sh
+++ exports/exports.sh
@@ -2,16 +2,29 @@
 
 # Java #
 
-export JAVA_HOME=/etc/alternatives/java_sdk_1.7.0_openjdk
-export PATH=$JAVA_HOME/bin:$PATH
+#export JAVA_HOME=/path/to/jdk
+#export CLASSPATH=
+
+#export PATH=$JAVA_HOME/bin:$PATH
 
 ## Apache Ant ##
 
-export ANT_HOME=/opt/devel/java/ant/apache-ant-1.9.2/
+#export ANT_HOME=/path/to/ant
+#export ANT_OPTS=
+
+#export PATH=$ANT_HOME/bin:$PATH
 
 ## Apache Maven ##
 
-export M2_HOME=/opt/devel/java/maven/apache-maven-3.1.0/
-export M2=$M2_HOME/bin
+#export M2_HOME=/path/to/maven
+#export M2=$M2_HOME/bin
 #export MAVEN_OPTS=
-export PATH=$M2:$PATH
+#export M2_REPO=
+
+#export PATH=$M2:$PATH
+
+# Oracle #
+
+#export ORACLE_HOME=/path/to/oracle
+#export ORACLE_SID=
+#export NLS_LANG=
